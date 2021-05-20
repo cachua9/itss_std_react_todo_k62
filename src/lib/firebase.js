89d1be71acd39de84897c358966e087900b923fb
Hoang Lam <meolam1999@gmail.com>
2021-05-20 16:33:33 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrtZZM3Uz1Q5BA1m3A_VXWjew1sQh4xXU",
+  authDomain: "fir-sample-8f101.firebaseapp.com",
+  projectId: "fir-sample-8f101",
+  storageBucket: "fir-sample-8f101.appspot.com",
+  messagingSenderId: "849241307917",
+  appId: "1:849241307917:web:11f9758b93910c04324e44"
+};
+
+firebase.initializeApp(firebaseConfig);
